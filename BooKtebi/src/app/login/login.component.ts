d641ad18d9ff5a2ed11450c261fedd95c0a09d30
mohamedamara1 <hamma.amara@gmail.com>
2021-03-20 16:05:46 +0100

    finished the html+css of login page, logic still needs to be implemented

--- BooKtebi/src/app/login/login.component.ts
+++ BooKtebi/src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-login',
@@ -6,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+	faCoffee = faCoffee;
 
   constructor() { }
 
