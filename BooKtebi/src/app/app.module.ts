bb2ef5e5734b8aaaa22a7e893af7b337837ca1c9
mohamedamara1 <hamma.amara@gmail.com>
2021-04-10 22:44:49 +0100

    merged landing page with login and signup page

--- BooKtebi/src/app/app.module.ts
+++ BooKtebi/src/app/app.module.ts
@@ -4,13 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginComponent } from './login/login.component';
-import { SignupComponent } from './signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GeneralsettingsComponent } from './pages/generalsettings/generalsettings.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, GeneralsettingsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ BooKtebi/src/app/pages/generalsettings/generalsettings.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GeneralsettingsComponent } from './generalsettings.component';
+
+describe('GeneralsettingsComponent', () => {
+  let component: GeneralsettingsComponent;
+  let fixture: ComponentFixture<GeneralsettingsComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ GeneralsettingsComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(GeneralsettingsComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
