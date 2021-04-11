0c961b4cc59b8be04afb128e51c7c03b67467098
mohamedamara1 <hamma.amara@gmail.com>
2021-04-11 20:56:08 +0100

    added components of general settings , payment method and delete account , still need to add routing between them

--- BooKtebi/src/app/app.module.ts
+++ BooKtebi/src/app/app.module.ts
@@ -9,9 +9,22 @@ import { SignupComponent } from './pages/signup/signup.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { GeneralsettingsComponent } from './pages/generalsettings/generalsettings.component';
+import { HomeComponent } from "./pages/home/home.component";
+import { HeaderComponent } from './components/header/header.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { NewProductsComponent } from './components/new-products/new-products.component';
+import { ProductComponent } from './components/product/product.component';
+import { BestSellingComponent } from './components/best-selling/best-selling.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LeftBarComponent } from './components/left-bar/left-bar.component';
+import { ProfileSettingsComponent } from './components/profile-settings/profile-settings.component';
+import { PaymentInfoComponent } from './components/payment-info/payment-info.component';
+import { DeleteAccountComponent } from './components/delete-account/delete-account.component';
 
 @NgModule({
-  declarations: [AppComponent, LoginComponent, SignupComponent, GeneralsettingsComponent],
+  declarations: [AppComponent, LoginComponent, SignupComponent, GeneralsettingsComponent,
+    HomeComponent, HeaderComponent, HeroComponent, NewProductsComponent,
+    ProductComponent, BestSellingComponent, FooterComponent, LeftBarComponent, ProfileSettingsComponent, PaymentInfoComponent, DeleteAccountComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
