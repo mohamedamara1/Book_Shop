d7cb2669f7416042fc61628125eb3091f0cf3358
mohamedamara1 <hamma.amara@gmail.com>
2021-04-12 09:39:47 +0100

    added routing for the components, works fine

--- BooKtebi/src/app/app-routing.module.ts
+++ BooKtebi/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DeleteAccountComponent } from './components/delete-account/delete-account.component';
+import { PaymentInfoComponent } from './components/payment-info/payment-info.component';
+import { ProfileSettingsComponent } from './components/profile-settings/profile-settings.component';
+import { GeneralsettingsComponent } from './pages/generalsettings/generalsettings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ // {path:'', component:GeneralsettingsComponent},
+  {
+    path:'generalsettings',
+   component:GeneralsettingsComponent,
+  children: [
+    {path:'profile-settings', component:ProfileSettingsComponent},
+    {path:'payment-method', component:PaymentInfoComponent},
+    {path:'delete-account', component:DeleteAccountComponent}
+        ]
+
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
