a49786572009d86848baf0acd89345dc36c64037
Chaima1Bf <chaima.benfarhat@isticbc.org>
2021-04-15 00:27:40 +0200

    update3

--- BooKtebi/src/app/app.module.ts
+++ BooKtebi/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { IonicModule } from "@ionic/angular’;";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -20,18 +20,22 @@ import { LeftBarComponent } from './components/left-bar/left-bar.component';
 import { ProfileSettingsComponent } from './components/profile-settings/profile-settings.component';
 import { PaymentInfoComponent } from './components/payment-info/payment-info.component';
 import { DeleteAccountComponent } from './components/delete-account/delete-account.component';
+import { ItemComponent } from './components/item/item.component';
+import { BasketComponent } from './components/basket/basket.component';
+
 
 @NgModule({
   declarations: [AppComponent, LoginComponent, SignupComponent, GeneralsettingsComponent,
     HomeComponent, HeaderComponent, HeroComponent, NewProductsComponent,
-    ProductComponent, BestSellingComponent, FooterComponent, LeftBarComponent, ProfileSettingsComponent, PaymentInfoComponent, DeleteAccountComponent],
+    ProductComponent, BestSellingComponent, FooterComponent, LeftBarComponent, ProfileSettingsComponent, PaymentInfoComponent, DeleteAccountComponent,ItemComponent,
+    BasketComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FontAwesomeModule,
     NgbModule,
-    IonicModule,
+    IonicModule
   ],
   providers: [],
   bootstrap: [AppComponent],
