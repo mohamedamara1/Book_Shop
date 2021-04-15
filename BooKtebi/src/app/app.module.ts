02bf51aef7bae74002ebcb1dae219518436f15e6
mohamedamara1 <hamma.amara@gmail.com>
2021-04-15 16:30:38 +0100

    fix typo

--- BooKtebi/src/app/app.module.ts
+++ BooKtebi/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { IonicModule } from "@ionic/angular’;";
+import { IonicModule } from "@ionic/angular";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
